dd2d7c1672103c57ec44007c60a07c4fbbfec65b
kodal <kodal@publicplan.de>
2024-03-16 18:09:07 +0100

    introduce and implement Webserver stub

--- firefly/classes/Server.py
+++ firefly/classes/Server.py
@@ -1,11 +1,23 @@
-import network
 import asyncio
 import socket
 from firefly.traits.Initializable import Initializable
 import asyncio
+from time import sleep
+from machine import Pin
 
 class Server(Initializable):
-    defaultResponse: str = f"""
+    led: Pin = None
+    s: socket = None
+    autoStart: bool = False
+    def __init__(self, requestHandler:function = None, **attributes):
+        Initializable.__init__(self, **attributes)
+        self.warmup()
+        self.led  = Pin("LED", Pin.OUT)
+        if (self.autoStart):
+            asyncio.gather(self.run(requestHandler))
+
+    def webpage(self, state = None):
+        html = f"""
             <!DOCTYPE html>
             <html>
             <head>
@@ -14,44 +26,69 @@ class Server(Initializable):
             </head>
             <body>
                 <h1>Raspberry Pi Pico Web Server</h1>
-                <p>It works!</p>
+                <h2>Led Control</h2>
+                <form action="./lighton">
+                    <input type="submit" value="Light on" />
+                </form>
+                <br>
+                <form action="./lightoff">
+                    <input type="submit" value="Light off" />
+                </form>
+                <p>LED state: {state}</p>
+                <h2>Fetch New Value</h2>
+                <form action="./value">
+                    <input type="submit" value="Fetch value" />
+                </form>
             </body>
             </html>
             """
-    async def defaultRequestHandler(reader, writer):
-        
-        print("Client connected")
-        request_line = await reader.readline()
-        print('Request:', request_line)
-        
-        # Skip HTTP request headers
-        while await reader.readline() != b"\r\n":
-            pass
-        
-        request = str(request_line, 'utf-8').split()[1]
-        print('Request:', request)
-        
-        # Process the request and update variables
-        response = "hello world!"
+        return str(html)
 
-        # Send the HTTP response and close the connection
-        writer.write('HTTP/1.0 200 OK\r\nContent-type: text/html\r\n\r\n')
-        writer.write(response)
-        await writer.drain()
-        await writer.wait_closed()
-        print('Client Disconnected')        
+    def warmup(self):
+        print('Starting server...')
+        addr = socket.getaddrinfo('0.0.0.0', 80)[0][-1]
+        self.s = socket.socket()
+        self.s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.s.bind(addr)
+        self.s.listen()
+        print('Server is istening on', addr)
 
-    requestHandler: function = None
-    def __init__(self, requestHandler:function = None, **attributes):
-        Initializable.__init__(self, **attributes)     
-        asyncio.run(self.run(requestHandler))
+    def run(self, requestHandler = None):
+        while True:
+            try:
+                conn, addr = self.s.accept()
+                print('Got a connection from', addr)
+                
+                # Receive and parse the request
+                request = conn.recv(1024)
+                request = str(request)
+                print('Request content = %s' % request)
 
-    async def run(self, requestHandler = None):
-        if (requestHandler is not None):
-            self.requestHandler = requestHandler
-        if (self.requestHandler is None):
-            def defaultRequestHandler():
-                return str(self.defaultResponse)
-            asyncio.start_server(defaultRequestHandler, "0.0.0.0", 80)
-        else:
-            asyncio.start_server(self.requestHandler, "0.0.0.0", 80)
+                try:
+                    request = request.split()[1]
+                    print('Request:', request)
+                except IndexError:
+                    pass
+                
+                # Process the request and update variables
+                if request == '/lighton?':
+                    print("LED on")
+                    self.led.value(1)
+                    state = "ON"
+                elif request == '/lightoff?':
+                    self.led.value(0)
+                    state = 'OFF'
+                
+                # Generate HTML response
+                response = self.webpage(self.led.value())  
+
+                # Send the HTTP response and close the connection
+                conn.send('HTTP/1.0 200 OK\r\nContent-type: text/html\r\n\r\n')
+                conn.send(response)
+                conn.close()
+
+            except OSError as e:
+                conn.close()
+                print('Connection closed')
+
+            sleep(0.1)
